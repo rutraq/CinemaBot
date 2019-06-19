7febbb55f6217e0bdcf06a0ab8cd7578f64f9009
Artur <a3332133@gmail.com>
2019-06-19 22:31:10 +0300

    Start with bot

--- ConsoleApp1/Telegram.cs
+++ ConsoleApp1/Telegram.cs
@@ -4,10 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace ConsoleApp1
 {
     class Telegram
     {
+        private static ITelegramBotClient botClient;
+        private static List<string> commands = new List<string>() { "/comands" };
+        private static string startText = "Привет я дурачёк";
+        public void Bot()
+        {
+            botClient = new TelegramBotClient("727728678:AAF4w9wmiNvU0f0VAH_UMBCYJjVIY8jwqkE") { Timeout = TimeSpan.FromSeconds(10) };
+            botClient.OnMessage += OnMessageAsync;
+            botClient.StartReceiving();
+        }
+
+        private async void OnMessageAsync(object sender, MessageEventArgs e)
+        {
+            var text = e?.Message?.Text;
+            if (text == null)
+            {
+                return;
+            }
+            else if (!commands.Contains(text))
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text: startText
+                    );
+            }
+        }
     }
 }

f056b23f3862183e443cd06b36e7bb5fcb120a4c
Artur <a3332133@gmail.com>
2019-06-19 23:51:39 +0300

    Search

--- ConsoleApp1/Telegram.cs
+++ ConsoleApp1/Telegram.cs
@@ -6,26 +6,21 @@ using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace ConsoleApp1
 {
     class Telegram
     {
         private static ITelegramBotClient botClient;
-        private static List<string> commands = new List<string>() { "/comands" };
-        private static string startText = "Привет я дурачёк";
+        private static List<string> commands = new List<string>() { "Вывод популярных фильмов", "Поиск фильма" };
+        private static string startText = "Нажмите какую-нибудь кнопку";
         public void Bot()
         {
             botClient = new TelegramBotClient("727728678:AAF4w9wmiNvU0f0VAH_UMBCYJjVIY8jwqkE") { Timeout = TimeSpan.FromSeconds(10) };
             botClient.OnMessage += OnMessageAsync;
             botClient.StartReceiving();
         }
-
-        public void fjlksdfjsl
-        {
-
-        }
-
         private async void OnMessageAsync(object sender, MessageEventArgs e)
         {
             var text = e?.Message?.Text;
@@ -35,15 +30,36 @@ namespace ConsoleApp1
             }
             else if (!commands.Contains(text))
             {
+                ReplyKeyboardMarkup replykeyboard = new ReplyKeyboardMarkup
+                (
+                    new[]
+                    {
+                        new[]
+                        {
+                            new KeyboardButton("Вывод популярных фильмов"),
+                            new KeyboardButton("Поиск фильма")
+                        }
+                    }
+                );
                 await botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
+                    chatId: e.Message.Chat, 
+                    replyMarkup: replykeyboard,
                     text: startText
                     );
             }
-        }
-        public void asdgfd()
-        {
-
+            //Популярные фильмы
+            else if (text == commands[0])
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat,
+                    text: "Популярные фильмы делает женя так что пока что идите нахуй"
+                    );
+            }
+            //Поиск фильма
+            else if (text == commands[1])
+            {
+                SearchFilm film = new SearchFilm("Докторс стрендж");
+            }
         }
     }
 }

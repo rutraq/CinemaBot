ce93bb4c2c29a9e5ff2a3c8f91801b000b9ba4af
Phanaty <38048498+Phanaty@users.noreply.github.com>
2019-06-20 00:03:09 +0300

    dsdsad

--- ConsoleApp1/Telegram.cs
+++ ConsoleApp1/Telegram.cs
@@ -13,17 +13,27 @@ namespace ConsoleApp1
     class Telegram
     {
         private static ITelegramBotClient botClient;
-        private static List<string> commands = new List<string>() { "Вывод популярных фильмов", "Поиск фильма" };
-        private static string startText = "Нажмите какую-нибудь кнопку";
+        private static List<string> commands = new List<string>() { "/comands" };
+        private static string startText = "Привет я дурачёк";
         public void Bot()
         {
             botClient = new TelegramBotClient("727728678:AAF4w9wmiNvU0f0VAH_UMBCYJjVIY8jwqkE") { Timeout = TimeSpan.FromSeconds(10) };
             botClient.OnMessage += OnMessageAsync;
             botClient.StartReceiving();
         }
+
+        public void fjlksdfjsl
+        {
+
+        }
+
         private async void OnMessageAsync(object sender, MessageEventArgs e)
         {
             var text = e?.Message?.Text;
+            for (int i = 0; i < films.Count; i++)
+            {
+                startText += films[i] + "\n";
+            }
             if (text == null)
             {
                 return;
